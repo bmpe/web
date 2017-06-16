0087bc67852abac3eb560d73844f27b86cda33f8
Naoki Tomii <naoki_tomii@bmpe.t.u-tokyo.ac.jp>
2017-06-16 09:08:20 +0900

    edit profile

--- profile/md2html.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-import glob
-import os
-
-files = glob.glob("*.md")
-for f in files:
-  cmd = "pandoc -c style.css -o {1} {0}".format(f, f.replace(".md",".html"))
-  print cmd
-  os.system(cmd)
